9be5a95b040837c5c8f4af6794dcbb18fcac1155
Zohaibkhan3020 <zohaibkhan3020@gmail.com>
2026-05-14 16:45:01 +0500

    .

--- src/app/services/menu.service.ts
+++ src/app/services/menu.service.ts
@@ -7,36 +7,17 @@ import { HttpClient } from '@angular/common/http';
 export class MenuService {
 
   private apiUrl = 'https://localhost:7033/api/auth';
+  private MenuUrl = 'https://localhost:7033/api/SecurityPermission';
 
   constructor(private http: HttpClient) {}
 
   getMenu(roleId: number, userId: number) {
     return this.http.get<any[]>(`${this.apiUrl}/menu?roleId=${roleId}&userId=${userId}`);
   }
-
-  getFiles() {
-        return Promise.resolve([
-            {
-                key: '0',
-                label: 'Documents',
-                children: [
-                    {
-                        key: '0-0',
-                        label: 'Work',
-                        children: [
-                            { key: '0-0-0', label: 'Expenses.doc' },
-                            { key: '0-0-1', label: 'Resume.doc' }
-                        ]
-                    }
-                ]
-            },
-            {
-                key: '1',
-                label: 'Pictures',
-                children: [
-                    { key: '1-0', label: 'Vacation.png' }
-                ]
-            }
-        ]);
-    }
+getPermissions(objectId: number, type: string, roleId: number, userId: number) {
+  return this.http.get<any>(
+    `${this.MenuUrl}/object-permissions?ObjectID=${objectId}&ObjectType=${type}&RoleID=${roleId}&UserID=${userId}`
+  );
+}
+ 
 }
